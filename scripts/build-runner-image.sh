5cb2f0fd1082a4aed20cae7986333ce52e765e40
David Lönnhager <dv.lnh.d@gmail.com>
2022-09-21 19:05:52 +0200

    Use e2tools to fill bootstrap image (no root)

--- scripts/build-runner-image.sh
+++ scripts/build-runner-image.sh
@@ -8,27 +8,19 @@ set -eu
 
 HARNESS_SIZE_MB=500
 HARNESS_IMAGE=test-runner.img
-HARNESS_MOUNT_POINT=/tmp/harness
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-function cleanup {
-    umount "${HARNESS_MOUNT_POINT}"
-}
-
-echo "Creating empty disk image for test harness"
+echo "Creating empty disk image for test runner"
 
 IMG_PATH="${SCRIPT_DIR}/../qemu-images/${HARNESS_IMAGE}"
 
-dd if=/dev/null of=${IMG_PATH} bs=1M seek="${HARNESS_SIZE_MB}"
+dd if=/dev/null of="${IMG_PATH}" bs=1M seek="${HARNESS_SIZE_MB}"
 mkfs.ext4 -F "${IMG_PATH}"
-
-mkdir -p "${HARNESS_MOUNT_POINT}"
-mount -t ext4 -o loop "${IMG_PATH}" "${HARNESS_MOUNT_POINT}"
-
-trap "cleanup" EXIT
 
 echo "Copying files to image"
 
-cp "${SCRIPT_DIR}/../target/x86_64-unknown-linux-gnu/release/test-tarpc" "${HARNESS_MOUNT_POINT}"
-cp "${SCRIPT_DIR}/../packages/"*.deb "${HARNESS_MOUNT_POINT}"
+e2cp \
+    "${SCRIPT_DIR}/../target/x86_64-unknown-linux-gnu/release/test-tarpc" \
+    "${SCRIPT_DIR}/../packages/"*.deb \
+    "${IMG_PATH}:/"
