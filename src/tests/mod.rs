bc59c309a6e5724b8bc794ae8c4adf85ab4f50b5
David Lönnhager <dv.lnh.d@gmail.com>
2022-09-21 20:01:48 +0200

    Verify system service status in install test

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 use tarpc::context;
 
-use crate::{package, ServiceClient};
+use crate::{app, package, ServiceClient};
 
 const APP_CURRENT_VERSION_PATH: &str = "/opt/testing/current-app.deb";
 const APP_PREVIOUS_VERSION_PATH: &str = "/opt/testing/previous-app.deb";
@@ -14,10 +14,24 @@ pub enum Error {
 
     #[error(display = "Package action failed")]
     PackageError(package::Error),
+
+    #[error(display = "Found running daemon unexpectedly")]
+    DaemonAlreadyRunning,
+
+    #[error(display = "Daemon unexpectedly not running")]
+    DaemonNotRunning,
 }
 
 pub async fn test_clean_app_install(rpc: ServiceClient) -> Result<(), Error> {
     // verify that daemon is not already running
+    if rpc
+        .poke_service(context::current())
+        .await
+        .map_err(Error::RpcError)?
+        != app::ServiceStatus::NotRunning
+    {
+        return Err(Error::DaemonAlreadyRunning);
+    }
 
     // install package
     rpc.install_app(
@@ -33,6 +47,14 @@ pub async fn test_clean_app_install(rpc: ServiceClient) -> Result<(), Error> {
     .map_err(|err| Error::PackageError(err))?;
 
     // verify that daemon is running
+    if rpc
+        .poke_service(context::current())
+        .await
+        .map_err(Error::RpcError)?
+        != app::ServiceStatus::Running
+    {
+        return Err(Error::DaemonNotRunning);
+    }
 
     Ok(())
 }
