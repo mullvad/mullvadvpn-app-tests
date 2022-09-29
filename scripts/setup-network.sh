f6346af75d374a9e74e3fd13f67b876fd66ecaaa
David Lönnhager <david.l@mullvad.net>
2022-09-29 10:09:46 +0200

    Forward traffic out of virtual network

--- scripts/setup-network.sh
+++ scripts/setup-network.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+VIRTUAL_NET=172.29.1.1/24
+
 set -eu
 
 ip link add br-mullvadtest type bridge
@@ -7,11 +9,21 @@ ip tuntap add tap-mullvadtest mode tap
 
 ip link set tap-mullvadtest master br-mullvadtest
 
-ip addr add dev br-mullvadtest 172.29.1.1/24
+ip addr add dev br-mullvadtest $VIRTUAL_NET
 
 ip link set br-mullvadtest up
 ip link set tap-mullvadtest up
 
-# In VM:
+# add NAT rule
+nft -f - <<EOF
+table ip mullvad_test_nat {
+    chain POSTROUTING {
+        type nat hook postrouting priority srcnat; policy accept;
+        ip saddr $VIRTUAL_NET ip daddr != $VIRTUAL_NET counter masquerade
+    }
+}
+EOF
+
+# In VM (example):
 #ip a add 172.29.1.2/24
 #ip r add default via 172.29.1.1
