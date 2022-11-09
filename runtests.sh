f9c7259363ee9605746dfae8bf48e802bf6b0495
David Lönnhager <david.l@mullvad.net>
2022-11-09 12:26:06 +0100

    Create a temporary TAP interface for each VM
    This makes it possible to run multiple tests in parallel

--- runtests.sh
+++ runtests.sh
@@ -41,18 +41,38 @@ case $TARGET in
 
 esac
 
+./build.sh
+
+function trap_handler {
+    if [[ -n "${QEMU_PID+x}" ]]; then
+        sudo kill -KILL -- $QEMU_PID >/dev/null 2>&1 || true
+    fi
+
+    if [[ -n "${HOST_NET_INTERFACE+x}" ]] &&
+          ip link show "${HOST_NET_INTERFACE}" >&/dev/null; then
+        echo "Removing interface ${HOST_NET_INTERFACE}"
+        sudo ip link del dev ${HOST_NET_INTERFACE}
+    fi
+}
+
+trap "trap_handler" EXIT TERM
+
 # Check if we need to setup the network
 ip link show br-mullvadtest >&/dev/null || sudo ./scripts/setup-network.sh
 
-./build.sh
+HOST_NET_INTERFACE=tap-mullvad$(cat /dev/urandom | tr -dc 'a-z' | head -c 4)
+
+echo "Creating network interface $HOST_NET_INTERFACE"
+
+sudo ip tuntap add ${HOST_NET_INTERFACE} mode tap
+sudo ip link set ${HOST_NET_INTERFACE} master br-mullvadtest
+sudo ip link set ${HOST_NET_INTERFACE} up
 
 echo "Compiling tests"
 
 cargo build -p test-manager
 
 echo "Launching guest VM"
-
-sudo echo
 
 pty=$(python3 -<<END_SCRIPT
 import os
@@ -69,15 +89,13 @@ sudo qemu-system-x86_64 -cpu host -accel kvm -m 2048 -smp 2 \
     -device usb-storage,drive=runner,bus=xhci.0 \
     -device virtio-serial-pci -serial pty \
     ${DISPLAY_ARG} \
-    -nic tap,ifname=tap-mullvadtest,script=no,downscript=no &
+    -nic tap,ifname=${HOST_NET_INTERFACE},script=no,downscript=no &
 
 QEMU_PID=$!
-
-trap "sudo kill -KILL -- $QEMU_PID >/dev/null 2>&1 || true" EXIT TERM
 
 echo "Executing tests"
 
 sleep 1
-sudo RUST_LOG=debug ACCOUNT_TOKEN=$ACCOUNT_TOKEN ./target/debug/test-manager ${pty} $@
+sudo RUST_LOG=debug ACCOUNT_TOKEN=${ACCOUNT_TOKEN} HOST_NET_INTERFACE=${HOST_NET_INTERFACE} ./target/debug/test-manager ${pty} $@
 
 sudo kill --timeout 5000 KILL -TERM -- $QEMU_PID >/dev/null 2>&1 || true
