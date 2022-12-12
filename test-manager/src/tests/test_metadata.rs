8c0f4f6a309487bad22b64b496c73ec91473c3b5
Jonathan <jonathan@mullvad.net>
2022-12-12 13:49:40 +0100

    Add cleanup after each test

--- test-manager/src/tests/test_metadata.rs
+++ test-manager/src/tests/test_metadata.rs
@@ -7,7 +7,7 @@ pub struct TestMetadata {
     pub command: &'static str,
     pub mullvad_client_version: MullvadClientVersion,
     pub func:
-        Box<dyn Fn(ServiceClient, Box<dyn std::any::Any>) -> BoxFuture<'static, Result<(), Error>>>,
+        Box<dyn Fn(ServiceClient, Box<dyn std::any::Any + Send>) -> BoxFuture<'static, Result<(), Error>>>,
     pub priority: Option<i32>,
 }
 
