af5ecfacb3c2f43ccf4e7a6faf9aac177869438c
David Lönnhager <david.l@mullvad.net>
2023-03-20 18:05:58 +0100

    Add OVMF and TPM options

--- test-manager/src/vm/qemu.rs
+++ test-manager/src/vm/qemu.rs
@@ -2,20 +2,24 @@ use crate::{
     config::{Config, VmConfig},
     vm::logging::forward_logs,
 };
+use async_tempfile::TempFile;
 use regex::Regex;
 use std::{
     io,
     net::IpAddr,
     ops::Deref,
+    path::PathBuf,
     process::{ExitStatus, Stdio},
     sync::Arc,
     time::Duration,
 };
 use tokio::{
+    fs,
     io::{AsyncBufReadExt, BufReader},
     process::{Child, Command},
     time::timeout,
 };
+use uuid::Uuid;
 
 use super::{network, VmInstance};
 
@@ -37,6 +41,18 @@ pub enum Error {
     NoPty,
     #[error(display = "Could not find IP address of guest")]
     NoIpAddr,
+    #[error(display = "Failed to copy OVMF vars")]
+    CopyOvmfVars(io::Error),
+    #[error(display = "Failed to wrap OVMF vars copy in tempfile object")]
+    WrapOvmfVars,
+    #[error(display = "Failed to start swtpm")]
+    StartTpmEmulator(io::Error),
+    #[error(display = "swtpm failed")]
+    TpmEmulator(io::Error),
+    #[error(display = "Timed out waiting for swtpm socket")]
+    TpmSocketTimeout,
+    #[error(display = "Failed to create temp dir")]
+    MkTempDir(io::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -57,6 +73,8 @@ pub struct QemuInstanceInner {
     pub ip_addr: IpAddr,
     child: Child,
     _network_handle: network::NetworkHandle,
+    _ovmf_handle: Option<OvmfHandle>,
+    _tpm_emulator: Option<TpmEmulator>,
 }
 
 #[async_trait::async_trait]
@@ -118,6 +136,27 @@ pub async fn run(config: &Config, vm_config: &VmConfig) -> Result<QemuInstance> 
         ]);
     }
 
+    let ovmf_handle;
+    let tpm_emulator;
+
+    // Configure OVMF. Currently, this is enabled implicitly if using a TPM
+    if vm_config.tpm {
+        let handle = OvmfHandle::new().await?;
+        handle.append_qemu_args(&mut qemu_cmd);
+        ovmf_handle = Some(handle);
+    } else {
+        ovmf_handle = None;
+    }
+
+    // Run software TPM emulator
+    if vm_config.tpm {
+        let handle = TpmEmulator::run().await?;
+        handle.append_qemu_args(&mut qemu_cmd);
+        tpm_emulator = Some(handle);
+    } else {
+        tpm_emulator = None;
+    }
+
     qemu_cmd.stdin(Stdio::piped());
     qemu_cmd.stdout(Stdio::piped());
     qemu_cmd.stderr(Stdio::piped());
@@ -157,7 +196,167 @@ pub async fn run(config: &Config, vm_config: &VmConfig) -> Result<QemuInstance> 
         ip_addr,
         child,
         _network_handle: network_handle,
+        _ovmf_handle: ovmf_handle,
+        _tpm_emulator: tpm_emulator,
     })))
+}
+
+/// Used to set up UEFI and append options to the QEMU command
+struct OvmfHandle {
+    temp_vars: TempFile,
+}
+
+impl OvmfHandle {
+    pub async fn new() -> Result<Self> {
+        const OVMF_VARS_PATH: &str = "/usr/share/OVMF/OVMF_VARS.secboot.fd";
+
+        // Create a local copy of OVMF_VARS
+        let temp_vars_path = random_tempfile_name();
+        fs::copy(OVMF_VARS_PATH, &temp_vars_path)
+            .await
+            .map_err(Error::CopyOvmfVars)?;
+
+        let temp_vars = TempFile::from_existing(temp_vars_path, async_tempfile::Ownership::Owned)
+            .await
+            .map_err(|_| Error::WrapOvmfVars)?;
+        Ok(OvmfHandle { temp_vars })
+    }
+
+    pub fn append_qemu_args(&self, qemu_cmd: &mut Command) {
+        const OVMF_CODE_PATH: &str = "/usr/share/OVMF/OVMF_CODE.secboot.fd";
+
+        qemu_cmd.args([
+            "-global",
+            "driver=cfi.pflash01,property=secure,value=on",
+            "-drive",
+            &format!("if=pflash,format=raw,unit=0,file={OVMF_CODE_PATH},readonly=on"),
+            "-drive",
+            &format!(
+                "if=pflash,format=raw,unit=1,file={}",
+                self.temp_vars.file_path().display()
+            ),
+            // Q35 supports secure boot
+            "-machine",
+            "q35,smm=on",
+        ]);
+    }
+}
+
+/// Runs a TPM emulator
+struct TpmEmulator {
+    handle: tokio::task::JoinHandle<Result<()>>,
+    sock_path: PathBuf,
+}
+
+impl TpmEmulator {
+    pub async fn run() -> Result<Self> {
+        let temp_dir = TempDir::new().await?;
+        let mut cmd = Command::new("swtpm");
+
+        let sock_path = temp_dir.0.join("tpmsock");
+
+        cmd.args([
+            "socket",
+            "-t",
+            "--ctrl",
+            &format!("type=unixio,path={}", sock_path.display()),
+            "--tpmstate",
+            &format!("dir={}", temp_dir.0.display()),
+            "--tpm2",
+        ]);
+
+        cmd.kill_on_drop(true);
+
+        cmd.stdout(Stdio::piped());
+        cmd.stderr(Stdio::piped());
+
+        // Start swtpm
+        let mut child = cmd.spawn().map_err(Error::StartTpmEmulator)?;
+
+        tokio::spawn(forward_logs(
+            "[swtpm] ",
+            child.stdout.take().unwrap(),
+            STDOUT_LOG_LEVEL,
+        ));
+        tokio::spawn(forward_logs(
+            "[swtpm] ",
+            child.stderr.take().unwrap(),
+            STDERR_LOG_LEVEL,
+        ));
+
+        let handle = tokio::spawn(async move {
+            let output = child.wait().await.map_err(Error::TpmEmulator)?;
+
+            if !output.success() {
+                log::error!("swtpm failed: {}", output);
+            }
+
+            temp_dir.delete().await;
+
+            Ok(())
+        });
+
+        const SOCKET_TIMEOUT: Duration = Duration::from_secs(10);
+
+        // Wait for socket to be created
+        timeout(SOCKET_TIMEOUT, async {
+            if sock_path.exists() {
+                return;
+            }
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        })
+        .await
+        .map_err(|_| {
+            handle.abort();
+            Error::TpmSocketTimeout
+        })?;
+
+        Ok(Self { handle, sock_path })
+    }
+
+    pub fn append_qemu_args(&self, qemu_cmd: &mut Command) {
+        qemu_cmd.args([
+            "-tpmdev",
+            "emulator,id=tpm0,chardev=chrtpm",
+            "-chardev",
+            &format!("socket,id=chrtpm,path={}", self.sock_path.display()),
+            "-device",
+            "tpm-tis,tpmdev=tpm0",
+        ]);
+    }
+}
+
+impl Drop for TpmEmulator {
+    fn drop(&mut self) {
+        self.handle.abort();
+    }
+}
+
+struct TempDir(PathBuf);
+
+impl TempDir {
+    pub async fn new() -> Result<Self> {
+        let temp_dir = std::env::temp_dir().join(Uuid::new_v4().to_string());
+        tokio::fs::create_dir_all(&temp_dir)
+            .await
+            .map_err(Error::MkTempDir)?;
+        Ok(Self(temp_dir))
+    }
+
+    pub async fn delete(self) {
+        let _ = fs::remove_dir_all(&self.0).await;
+        std::mem::forget(self);
+    }
+}
+
+impl Drop for TempDir {
+    fn drop(&mut self) {
+        let _ = std::fs::remove_dir_all(&self.0);
+    }
+}
+
+fn random_tempfile_name() -> PathBuf {
+    std::env::temp_dir().join(format!("tmp{}", Uuid::new_v4().to_string()))
 }
 
 async fn find_pty(process: &mut tokio::process::Child) -> Result<String> {
