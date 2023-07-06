8a25fcdd6f50104b9de6c82f06aa4e715a5a84e9
Markus Pettersson <markus.pettersson@mullvad.net>
2023-07-06 11:48:21 +0200

    Implement a generic connection check strategy
    As part of our automated tests, we want to assure that the internet is
    reachable through the VPN tunnel. To verify connectivity, we can request
    internet resources which are not the currently connected relay. Also,
    try something more elaborate than a ping (i.e. check for HTTP `200` from
    `am.i.mullvad.net`).
    
    This is prone to network failures, which is why we retry each request 3
    times before we consider it a failure.

--- test-manager/src/tests/helpers.rs
+++ test-manager/src/tests/helpers.rs
@@ -1,10 +1,7 @@
 use super::{config::TEST_CONFIG, Error, PING_TIMEOUT, WAIT_FOR_TUNNEL_STATE_TIMEOUT};
 use crate::network_monitor::{start_packet_monitor, MonitorOptions};
 use futures::StreamExt;
-use mullvad_management_interface::{
-    types::{self, RelayLocation},
-    ManagementServiceClient,
-};
+use mullvad_management_interface::{types, ManagementServiceClient};
 use mullvad_types::{
     relay_constraints::{
         Constraint, GeographicLocationConstraint, LocationConstraint, OpenVpnConstraints,
@@ -23,7 +20,8 @@ use talpid_types::net::{
     IpVersion, TunnelType,
 };
 use test_rpc::{
-    mullvad_daemon::ServiceStatus, package::Package, AmIMullvad, Interface, ServiceClient,
+    mullvad_daemon::ServiceStatus, net::http_get_with_retries, package::Package, AmIMullvad,
+    Interface, ServiceClient,
 };
 use tokio::time::timeout;
 
@@ -342,7 +340,6 @@ pub async fn wait_for_mullvad_service_state(
 pub async fn geoip_lookup_with_retries(rpc: ServiceClient) -> Result<AmIMullvad, Error> {
     const MAX_ATTEMPTS: usize = 5;
     const BEFORE_RETRY_DELAY: Duration = Duration::from_secs(2);
-
     let mut attempt = 0;
 
     loop {
@@ -420,8 +417,7 @@ pub async fn update_relay_settings(
                 types::NormalRelaySettingsUpdate {
                     location: constraints
                         .location
-                        .map(Constraint::option)
-                        .flatten()
+                        .and_then(Constraint::option)
                         .map(types::LocationConstraint::from),
                     providers: constraints
                         .providers
