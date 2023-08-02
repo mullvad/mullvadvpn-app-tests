b92276a6c492af6bfd8faef4d444d2aac042b877
Markus Pettersson <markus.pettersson@mullvad.net>
2023-08-02 12:32:16 +0200

    Actually create array of OSes in CI script
    Previously, the array of OSes would be interpreted as a string, which
    would make the `os` argument of the `download_e2e_executable` function
    be equal to the string of all `TEST_OSES`

--- ci-runtests.sh
+++ ci-runtests.sh
@@ -29,7 +29,8 @@ echo "* Version to upgrade from: $OLD_APP_VERSION"
 echo "* Version to test: $NEW_APP_VERSION"
 echo "**********************************"
 
-TEST_OSES=(${TEST_OSES-"debian11 debian12 ubuntu2004 ubuntu2204 ubuntu2304 fedora38 fedora37 fedora36 windows10 windows11"})
+DEFAULT_OSES=(debian11 debian12 ubuntu2004 ubuntu2204 ubuntu2304 fedora38 fedora37 fedora36 windows10 windows11)
+TEST_OSES=( "${TEST_OSES[@]:-"${DEFAULT_OSES[@]}"}" )
 
 if [[ -z "${ACCOUNT_TOKENS+x}" ]]; then
     echo "'ACCOUNT_TOKENS' must be specified" 1>&2
